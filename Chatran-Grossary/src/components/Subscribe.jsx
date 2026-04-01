26eef143707ff34b3e557ad6d9423fe01963038f
DKRoaunak <rounakgupta8854@gmail.com>
2026-04-01 15:15:24 +0530

    Add Seller Login Page

---
+++ Chatran-Grossary/src/components/Subscribe.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Example() {
+    return (
+
+        <div className="w-full bg-gray-50 rounded-2xl px-2 text-center text-white py-20 flex flex-col items-center justify-center mt-10">
+            <p className="text-primary-lightgreen font-medium">Get updated</p>
+            <h1 className="max-w-lg font-semibold text-4xl/[44px] mt-2 text-primary-darkgreen">Subscribe to our Chatran Grossary & Get the Best Deals</h1>
+            <div className="flex items-center justify-center mt-10 border border-primary-lightgreen focus-within:outline focus-within:outline-primary-lightgreen text-sm rounded-full h-14 max-w-md w-full">
+                <input type="text" className="bg-transparent text-primary-darkgreen outline-none rounded-full px-4 h-full flex-1" placeholder="Enter your email address" />
+                <button className="rounded-full h-11 mr-1 px-8 flex items-center justify-center bg-primary-lightgreen hover:bg-primary-darkgreen transition text-primary-darkgreen hover:text-primary-lightgreen">
+                    Subscribe now
+                </button>
+            </div>
+        </div>
+    );
+};
---
+++ Chatran-Grossary/src/pages/AllProducts.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from 'react'
+import { useAppContext } from '../context/AppContext'
+import ProductCard from '../components/ProductCard'
+import { useState } from 'react';
+
+const AllProducts = () => {
+
+    const { products, searchQuery } = useAppContext();
+    const [filteredProducts, setFilteredProducts] = useState([]);
+
+    useEffect(() => {
+
+        try {
+            if (searchQuery.length > 0) {
+                setFilteredProducts(products.filter(product => product.name.toLowerCase().includes(searchQuery.toLowerCase())
+                ))
+            } else {
+                setFilteredProducts(products);
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }, [products, searchQuery]);
+
+    return (
+        <div className='mt-16 flex flex-col'>
+            <div className='flex flex-col items-end w-max'>
+                <p className='text-2xl font-medium uppercase'>All Products</p>
+                <div className='w-16 h-0.5 bg-primary-lightgreen rounded-full'></div>
+            </div>
+            <div className='mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-16'>
+                {filteredProducts.filter((product) => product.inStock).map((product, index) => (
+                    <ProductCard key={index} product={product} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default AllProducts
